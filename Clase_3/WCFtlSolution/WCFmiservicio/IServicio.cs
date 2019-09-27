fd6fa46bdf2f3ad52b13103f5d2a17fe10e9500b
Diego Rubi Salas <43016665+Diegoalru@users.noreply.github.com>
2019-09-27 16:52:45 -0600

    Practica #1 y #2 agregadas.

--- Clase_3/WCFtlSolution/WCFmiservicio/IServicio.cs
+++ Clase_3/WCFtlSolution/WCFmiservicio/IServicio.cs
@@ -13,5 +13,44 @@ namespace WCFmiservicio
     {
         [OperationContract]
         int GetSuma(int num1, int num2);
+
+        [OperationContract]
+        int GetResta(int num1, int num2);
+
+        /// <summary>
+        /// Operacion que devuelve el resultado de una división.
+        /// </summary>
+        /// <param name="num1">Primer numero.</param>
+        /// <param name="num2">Segundo numero.</param>
+        /// <returns>Retorna el resultado de la operación de division.</returns>
+        [OperationContract]
+        double GetDivision(int num1, int num2);
+
+        /// <summary>
+        /// Operacion que devuelve el resultado de una multiplicación.
+        /// </summary>
+        /// <param name="num1">Primer numero.</param>
+        /// <param name="num2">Segundo numero.</param>
+        /// <returns>Retorna el resultado de la operación de multiplicación.</returns>
+        [OperationContract]
+        int GetMultiplicacion(int num1, int num2);
+
+        /// <summary>
+        /// Operacion que devuelve el modulo de una división.
+        /// </summary>
+        /// <param name="num1">Primer numero.</param>
+        /// <param name="num2">Segundo numero.</param>
+        /// <returns>Retorna el resultado de la operación de modulo.</returns>
+        [OperationContract]
+        int GetModulo(int num1, int num2);
+
+        [OperationContract]
+        string Saludar(string nombre);
+
+        [OperationContract]
+        bool Login(string usuario, string clave);
+
+        [OperationContract]
+        double GetDivide(int num1, int num2);
     }
 }
--- Clase_3/WCFtlSolution/WCFmiservicio/Servicio.svc.cs
+++ Clase_3/WCFtlSolution/WCFmiservicio/Servicio.svc.cs
@@ -11,10 +11,84 @@ namespace WCFmiservicio
     // NOTE: In order to launch WCF Test Client for testing this service, please select Servicio.svc or Servicio.svc.cs at the Solution Explorer and start debugging.
     public class Servicio : IServicio
     {
+        #region Basicos
+
         public int GetSuma(int num1, int num2)
         {
-            int resultado = num1 + num2;
-            return resultado;
+            return num1 + num2; ;
         }
+
+        public int GetResta(int num1, int num2)
+        {
+            return num1 - num2; ;
+        }
+
+        public double GetDivision(int num1, int num2)
+        {
+            return (double)num1 / num2;
+        }
+
+        public int GetMultiplicacion(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int GetModulo(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+
+        #endregion
+
+        #region Practica 1
+
+        public string Saludar(string nombre)
+        {
+            string saludo = "Hola ";
+            saludo += nombre;
+            return saludo;
+        }
+
+        public bool Login(string usuario, string clave)
+        {
+            //Validaciones:
+            if (EsVacio(usuario)) return false;
+            if (EsVacio(clave)) return false;
+
+            if (usuario.Equals("Admin") && clave.Equals("987"))
+                return true;
+            return false;
+        }
+
+        #endregion
+
+        #region Practica 2
+
+        private int Indica0(int num)
+        {
+            return (num == 0) ? -999 : num;
+        }
+
+        public double GetDivide(int num1, int num2)
+        {
+            int divisor = Indica0(num2);
+            if (divisor != -999)
+            {
+                return divisor;
+            }
+            else
+            {
+                return GetDivision(num1, num2);
+            }
+        }
+
+        private bool EsVacio(string texto)
+        {
+            if (texto.Equals("") || texto == null)
+                return true;
+            return false;
+        }
+
+        #endregion
     }
 }
