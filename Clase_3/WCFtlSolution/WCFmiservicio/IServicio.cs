c26756b6657e4a4431d8b8488361580cfc3c49a8
Diego Rubi Salas <43016665+Diegoalru@users.noreply.github.com>
2019-09-27 12:06:00 -0600

    Clase 3 Proyecto ejemplo agregado.

---
+++ Clase_3/WCFtlSolution/WCFmiservicio/IServicio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFmiservicio
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServicio" in both code and config file together.
+    [ServiceContract]
+    public interface IServicio
+    {
+        [OperationContract]
+        int GetSuma(int num1, int num2);
+    }
+}
